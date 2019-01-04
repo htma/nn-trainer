7e7929ba7e4f9a3e54c6ebbfd1417d4ecb0a6323
htma <mht0003@gmail.com>
2019-01-04 16:10:42 +0800

    add generate_data.py to commit

---
+++ generate_data.py
@@ -0,0 +1,34 @@
+# generate random points within a range, such as circle or rectangle.
+# created on Dec 25, 2018 by mht
+# revised on Jan 2, 2019
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+num_samples = 1000
+
+# make a simple unit circle 
+theta = np.linspace(0, 2*np.pi, num_samples)
+a, b = 1 * np.cos(theta), 1 * np.sin(theta)
+
+# generate the points within a circle
+r = np.random.rand((num_samples))
+xc, yc = r * np.cos(theta), r * np.sin(theta)
+
+
+# generate the points within a rectangle
+xr = -1.5+ np.random.rand(1,1000)*3
+yr = -1.5+ np.random.rand(1,1000)*3
+
+# plots
+plt.figure(figsize=(7,6))
+plt.plot(a, b, linestyle='-', linewidth=2, label='Circle')
+plt.plot(xr, yr, marker='o', linestyle='')
+#plt.plot(xc, yc, marker='o', linestyle='')
+plt.ylim([-1.5,1.5])
+plt.xlim([-1.5,1.5])
+plt.grid()
+#plt.legend(loc='upper right')
+plt.show(block=True)
+
