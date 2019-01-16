dc2e99fddb65a61deab420e54ad95515fac5e531
Haitao Ma <mht0003@gmail.com>
2019-01-16 10:44:41 +0800

    generate_data.py can write data into a file

--- generate_data.py
+++ generate_data.py
@@ -1,26 +1,45 @@
 # generate random points within a range, such as circle or rectangle.
 # created on Dec 25, 2018 by mht
-# revised on Jan 2, 2019
 
-import numpy as np 
+import math
+import numpy as np
 import matplotlib.pyplot as plt 
 
 
-num_samples = 100
+num_samples = 10 # number of points 
 
 # make a simple unit circle 
-theta = np.linspace(0, 2*np.pi, num_samples)
+theta = np.linspace(0, 2*np.pi, 100)
 a, b = 1 * np.cos(theta), 1 * np.sin(theta)
 
 # generate the points within a circle
-r = np.random.rand((num_samples))
-xc, yc = r * np.cos(theta), r * np.sin(theta)
+#r = np.random.rand((num_samples))
+#xc, yc = r * np.cos(theta), r * np.sin(theta)
 
 
-# generate the points within a rectangle
-xr = -1.5+ np.random.rand(1,1000)*3
-yr = -1.5+ np.random.rand(1,1000)*3
+# generate the num_samples points within a rectangle
 
+xr = -1.5+ np.random.rand(1,num_samples)*3
+yr = -1.5+ np.random.rand(1,num_samples)*3
+points = list(zip(xr[0], yr[0]))
+
+
+#        print('The label of the point {} is {:}'.format((point[0], point[1]), 1))
+ 
+
+# write the data to a file
+with open('data/dataset.csv','w') as output_file:
+    for point in points:
+        if math.sqrt(point[0]**2+point[1]**2) <= 1:
+            output_file.write(str(point[0])+ ",")
+            output_file.write(str(point[1])+ ",")
+            output_file.write(str(1)+"\n")
+        else:
+            output_file.write(str(point[0])+ ",")
+            output_file.write(str(point[1])+ ",")
+            output_file.write(str(0)+"\n")
+output_file.close()
+        
 # plots
 plt.figure(figsize=(7,6))
 plt.plot(a, b, linestyle='-', linewidth=2, label='Circle')
