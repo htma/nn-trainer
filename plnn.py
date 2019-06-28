a6079cad250331b532f7c9591470f48c80a69713
Haitao Ma <mht0003@gmail.com>
2019-06-28 10:27:22 +0800

    adding state computation.

--- plnn.py
+++ plnn.py
@@ -11,10 +11,11 @@ from torchvision import datasets, transforms
 
 from data_loader import MyCustomDataset
 
-from hidden_filter import hid_filter, build_RGB
+from hidden_neuron_status import neuron_active, build_RGB
 #from generate_data import generate_batches
 
 dtype = torch.float
+batch_size = 10
 device = torch.device("cpu")
 
 class PLNN(torch.nn.Module):
@@ -34,31 +35,35 @@ class PLNN(torch.nn.Module):
         
 
     def forward(self, x):
-        state = [] # the states of all hidden layers
+        #state = np.zeros((batch_size, 2)) # the states of all hidden layers
         # Forward pass
         h1 = self.hidden1(x)
-        h1 = hid_filter(h1)
-        state.extend(h1.detach().numpy()[0])
+        h1_state = neuron_active(h1)
+    
         h1_relu = h1.clamp(min=0)
         h2 = self.hidden2(h1_relu)
-        h2 = hid_filter(h2)
-        state.extend(h2.detach().numpy()[0])
+        h2_state = neuron_active(h2)
+        state = np.concatenate((h1_state, h2_state), axis=1)
+        print('state2 shape is ', state.shape)
+        
         h2_relu = h2.clamp(min=0)
         h3 = self.hidden3(h2_relu)
-        h3 = hid_filter(h3)
-        state.extend(h3.detach().numpy()[0])
+        h3_state = neuron_active(h3)
+        state = np.concatenate((state, h3_state), axis=1)
+        print('state3 shape is ', state.shape)
+
         h3_relu = h3.clamp(min=0)
         y_pred = self.output(h3_relu)
-        state = list(map(int, state))
         
-        return state, F.log_softmax(y_pred)
+        return state, F.log_softmax(y_pred, dim=1)
 
 def main():
-    N, D_in, D_out = 10, 2, 2
+    D_in, D_out = 2, 2
     H1, H2, H3 = 4, 16, 2
     model = PLNN(D_in, H1, H2, H3, D_out)
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
     criterion = nn.NLLLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+   
     
     print(model)
     #batches = generate_batches(N)
@@ -67,46 +72,44 @@ def main():
         MyCustomDataset('./data/dataset.csv',
                         transform=transforms.Compose([
                             transforms.ToTensor()])),
-        batch_size=200,shuffle=False)    
+        batch_size=10,shuffle=False)    
 
     # define  an unit circle
     theta = np.linspace(0, 2*np.pi, 100)
     a, b = 1 * np.cos(theta), 1 * np.sin(theta)
   
     # training
-    for epoch in range(3):
+    for epoch in range(10):
         fig, ax = plt.subplots()
         for batch_idx, (data, target)  in enumerate(train_loader):
-            data, target = Variable(data), Variable(target)
+            data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
             state,predictions = model(data)
+            print('state is ', state)
             loss = criterion(predictions, target)
             loss.backward()
             optimizer.step()
 
+            # printing training results
+
+            # if batch_idx % 10 == 0:
+            #     print('Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            #         epoch, batch_idx*len(data), len(train_loader.dataset), 100.*batch_idx/len(train_loader),  loss.item()))
+
+                
             # painting training  results
             ndata = data.numpy()
-            print('ndata is ', ndata)
             for x in ndata:
                 rect = plt.Rectangle(x, 0.1, 0.1, fc=build_RGB(state))
                 ax.add_patch(rect)
 
-            plt.plot(a, b, linestyle='-', linewidth=2, label='Circle')
-            ax.set(xlabel='$x_1$', ylabel='$x_2$', title='Train data')
-            ax.xaxis.set_ticks([-1.5, -1.2, -0.9, -0.6,-0.3, 0, 0.3,0.6,0.9,1.2,1.5])
-            ax.yaxis.set_ticks([-1.5, -1.2, -0.9, -0.6,-0.3, 0, 0.3,0.6,0.9,1.2,1.5])
+        plt.plot(a, b, linestyle='-', linewidth=2, label='Circle')
+        ax.set(xlabel='$x_1$', ylabel='$x_2$', title='Train data')
+        ax.xaxis.set_ticks([-1.5, -1.2, -0.9, -0.6,-0.3, 0, 0.3,0.6,0.9,1.2,1.5])
+        ax.yaxis.set_ticks([-1.5, -1.2, -0.9, -0.6,-0.3, 0, 0.3,0.6,0.9,1.2,1.5])
     
-            ax.grid(True)
-            plt.show()
-
-            # printing training results
-            if batch_idx % 10 == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx*len(data),
-                    len(train_loader.dataset),
-                    100. * batch_idx/len(train_loader),
-                    loss.data[0]))
-
+        ax.grid(True)
+#        plt.show()
 
 if __name__ == '__main__':
     main()
