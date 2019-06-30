a2bef622f2d2238775ce2d0d4ec9cbfd0d4ad148
Haitao Ma <mht0003@gmail.com>
2019-06-30 09:42:34 +0800

    Adding state dict computation.

--- plnn.py
+++ plnn.py
@@ -35,28 +35,38 @@ class PLNN(torch.nn.Module):
         
 
     def forward(self, x):
-        #state = np.zeros((batch_size, 2)) # the states of all hidden layers
         # Forward pass
         h1 = self.hidden1(x)
         h1_state = neuron_active(h1)
-    
+    #    print('state_h1 shape is ', h1_state.shape)
         h1_relu = h1.clamp(min=0)
         h2 = self.hidden2(h1_relu)
         h2_state = neuron_active(h2)
-        state = np.concatenate((h1_state, h2_state), axis=1)
-        print('state2 shape is ', state.shape)
+        # states: hidden layer active states, i.e., configurations
+        states = np.concatenate((h1_state, h2_state), axis=1)
+   #     print('state_h2 shape is ', states.shape)
         
         h2_relu = h2.clamp(min=0)
         h3 = self.hidden3(h2_relu)
         h3_state = neuron_active(h3)
-        state = np.concatenate((state, h3_state), axis=1)
-        print('state3 shape is ', state.shape)
+        states = np.concatenate((states, h3_state), axis=1)
+  #      print('state_h3 shape is ', states.shape)
 
         h3_relu = h3.clamp(min=0)
         y_pred = self.output(h3_relu)
         
-        return state, F.log_softmax(y_pred, dim=1)
+        return states, F.log_softmax(y_pred, dim=1)
 
+def store_states(states):
+    ''' Store states into a dict.'''
+    result = dict()
+    for i in range(states.shape[0]):
+        state = int(''.join(list(map(str, states[0, :]))), 2)
+        if state not in result.values():
+            result[i] = state
+
+    return result
+        
 def main():
     D_in, D_out = 2, 2
     H1, H2, H3 = 4, 16, 2
@@ -79,13 +89,19 @@ def main():
     a, b = 1 * np.cos(theta), 1 * np.sin(theta)
   
     # training
-    for epoch in range(10):
+    state_dict = dict()
+    for epoch in range(2):
         fig, ax = plt.subplots()
+
         for batch_idx, (data, target)  in enumerate(train_loader):
             data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
-            state,predictions = model(data)
-            print('state is ', state)
+            states,predictions = model(data)
+            for i in range(states.shape[0]):
+                state = int(''.join(list(map(str, states[0,:]))), 2)
+                if state not in state_dict.values():
+                    state_dict[str(epoch)+str(batch_idx)+str(i)] = state
+            print('state dict is:  ', state_dict)
             loss = criterion(predictions, target)
             loss.backward()
             optimizer.step()
@@ -97,19 +113,20 @@ def main():
             #         epoch, batch_idx*len(data), len(train_loader.dataset), 100.*batch_idx/len(train_loader),  loss.item()))
 
                 
-            # painting training  results
-            ndata = data.numpy()
-            for x in ndata:
-                rect = plt.Rectangle(x, 0.1, 0.1, fc=build_RGB(state))
-                ax.add_patch(rect)
+           #  # painting training  results
+#             ndata = data.numpy()
+#             for x in ndata:
+#                 rect = plt.Rectangle(x, 0.1, 0.1, fc=build_RGB(state))
+#                 ax.add_patch(rect)
 
-        plt.plot(a, b, linestyle='-', linewidth=2, label='Circle')
-        ax.set(xlabel='$x_1$', ylabel='$x_2$', title='Train data')
-        ax.xaxis.set_ticks([-1.5, -1.2, -0.9, -0.6,-0.3, 0, 0.3,0.6,0.9,1.2,1.5])
-        ax.yaxis.set_ticks([-1.5, -1.2, -0.9, -0.6,-0.3, 0, 0.3,0.6,0.9,1.2,1.5])
+#         plt.plot(a, b, linestyle='-', linewidth=2, label='Circle')
+#         ax.set(xlabel='$x_1$', ylabel='$x_2$', title='Train data')
+#         ax.xaxis.set_ticks([-1.5, -1.2, -0.9, -0.6,-0.3, 0, 0.3,0.6,0.9,1.2,1.5])
+#         ax.yaxis.set_ticks([-1.5, -1.2, -0.9, -0.6,-0.3, 0, 0.3,0.6,0.9,1.2,1.5])
     
-        ax.grid(True)
-#        plt.show()
+#         ax.grid(True)
+# #        plt.show()
 
 if __name__ == '__main__':
     main()
+
