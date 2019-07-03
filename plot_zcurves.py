93aa66595358716f9b4afeeee37d5221b38a1b55
Haitao Ma <mht0003@gmail.com>
2019-07-03 10:36:22 +0800

    Complete plot_zcurves file.

--- plot_zcurves.py
+++ plot_zcurves.py
@@ -2,30 +2,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 
+def plot_zcurve(x,y):
+    '''x: tuple of x corordinate (x1, x2).
+       y: tuple of y corordinate (y1, y2).
+    '''
+    x = np.concatenate((x,x), axis=None)
+    y = list(zip(y[::-1], y[::-1]))
+    y = [item for tup in y for item in tup]
+    return x, y
+    
 #x = np.array((-0.75,0.75,-0.75,0.75))
 #y = np.array((0.75,0.75,-0.75,-0.75))
-k = 16 # number of points every axis
-x = np.linspace(-1.5+3/2**k, 1.5-3/2**k, k)
-y = np.linspace(-1.5+3/2**k, 1.5-3/2**k, k)
-#x = np.concatenate((x,x), axis=None)
-#y = list(zip(y[::-1],y[::-1]))
-#y = [item for t in y for item in t]
-xy = list(zip(*itertools.product(x,y)))
-x, y = xy[0], xy[1]
-print(x)
-print(y)
+k = 2 # number of points every axis
+x = np.linspace(-1.5+3/2**(k+1), 1.5-3/2**(k+1), k**2)
+y = np.linspace(-1.5+3/2**(k+1), 1.5-3/2**(k+1), k**2)
+x_left, y_left = plot_zcurve(x[:2],y[:2])
+x_left, y_right = plot_zcurve(x[:2],y[2:])
+x_right, y_left = plot_zcurve(x[2:],y[:2])
+x_right, y_right = plot_zcurve(x[2:],y[2:])
+
 
 # Lines on top of scatter
 fig, ax = plt.subplots()
 #plt.subplot(211)
-plt.plot(x, y, 'r', lw=3)
-plt.scatter(x, y, s=120)
-plt.axis([-1.5, 1.5, -1.5, 1.5])
-ax.set(xlabel='$x_1$', ylabel='$x_2$', title='Z curve')
-plt.grid()
-plt.legend(loc='upper right')
+plt.plot(x_left, y_left, 'r', lw=3)
+plt.scatter(x_left, y_left, s=120)
 
-plt.title('Lines on top of dots')
+plt.plot([x_left[-1], x_right[0]], [y_right[-1], y_right[0]], 'black', lw=3)
+plt.plot(x_left, y_right, 'r', lw=3)
+plt.scatter(x_left, y_right, s=120)
+plt.plot([x_right[-1], x_left[0]], [y_right[-1], y_left[0]], 'black', lw=3)
+plt.plot(x_right, y_left, 'r', lw=3)
+plt.scatter(x_right, y_left, s=120)
+plt.plot([x_left[-1], x_right[0]], [y_left[-1], y_left[0]], 'black', lw=3)
+plt.plot(x_right, y_right, 'r', lw=3)
+plt.scatter(x_right, y_right, s=120)
+
+plt.plot([-1.5, 1.5], [0, 0], '--', lw=1)
+plt.plot([0,0],[-1.5,1.5], '--',  lw=1)
+plt.axis([-1.5, 1.5, -1.5, 1.5])
+ax.set(xlabel='$x$', ylabel='$y$', title='Data Z-curve Distribution')
+#plt.grid()
+#plt.legend(loc='upper right')
+
+#plt.title('Lines on top of dots')
 
 # # Scatter plot on top of lines
 # plt.subplot(212)
@@ -33,14 +53,5 @@ plt.title('Lines on top of dots')
 # plt.scatter(x, y, s=120, zorder=2)
 # plt.title('Dots on top of lines')
 
-# A new figure, with individually ordered items
-# x = np.linspace(0, 2*np.pi, 100)
-# plt.figure()
-# plt.plot(x, np.sin(x), linewidth=10, color='black', label='zorder=10', zorder=10)  # on top
-# plt.plot(x, np.cos(1.3*x), linewidth=10, color='red', label='zorder=1', zorder=1)  # bottom
-# plt.plot(x, np.sin(2.1*x), linewidth=10, color='green', label='zorder=3', zorder=3)
-# plt.axhline(0, linewidth=10, color='blue', label='zorder=2', zorder=2)
-# plt.title('Custom order of elements')
-# l = plt.legend()
-# l.set_zorder(20)  # put the legend on top
+
 plt.show()
